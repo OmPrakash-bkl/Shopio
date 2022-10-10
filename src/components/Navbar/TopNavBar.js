c438bf9e347b98ea837d1dc2f90dddb4711ad009
prakash_web_dev <omprakashgwt@gmail.com>
2022-10-10 14:46:40 +0530

    created home page header 100%

--- src/components/Navbar/TopNavBar.js
+++ src/components/Navbar/TopNavBar.js
@@ -5,11 +5,11 @@ function TopNavBar() {
   return (
   <Container fluid className={styles.topNavBarContainer}>
   <div className="hstack gap-3">
-  <div className="d-md-none d-sm-none d-none d-lg-block"><h5><a href="#" className="text-decoration-none text-light">Successful Ecommerce, Let's Start Today!</a></h5></div>
+  <div className="d-md-none d-sm-none d-none d-lg-block"><h5><a href="#" className={`text-decoration-none ${styles.topNavBarSlogan}`}>Successful Ecommerce, Let's Start Today!</a></h5></div>
   <div className="ms-auto d-md-none fs-6 d-sm-none d-none d-lg-block">
-  <a href="#" className="mx-2 text-light"><i className="fa fa-facebook-f"></i></a>
-  <a href="#" className="mx-2 text-light"><i className="fa fa-envelope"></i></a>
-  <a href="#" className="mx-2 text-light"><i className="fa fa-whatsapp"></i></a>
+  <a href="#" className="mx-2 text-light"><i className={`fa fa-facebook-f ${styles.socialIconStyles}`}></i></a>
+  <a href="#" className="mx-2 text-light"><i className={`fa fa-envelope ${styles.socialIconStyles}`}></i></a>
+  <a href="#" className="mx-2 text-light"><i className={`fa fa-whatsapp ${styles.socialIconStyles}`}></i></a>
   </div>
   <div className="vr d-md-none text-light d-sm-none d-none d-lg-block"></div>
   <div className="d-md-none text-light d-sm-none d-none d-lg-block">
