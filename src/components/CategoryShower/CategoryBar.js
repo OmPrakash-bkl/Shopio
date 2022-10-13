efc85b8f9110eb4db5d643906b46888d2ff44c46
prakash_web_dev <omprakashgwt@gmail.com>
2022-10-13 17:27:46 +0530

    created dropdown container 50%

--- src/components/CategoryShower/CategoryBar.js
+++ src/components/CategoryShower/CategoryBar.js
@@ -1,28 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
-import Dropdown from 'react-bootstrap/Dropdown';
 import styles from './CategoryBar.module.css';
 
 function CategoryBar() {
+
+  const [showDropdown, setshowDropdown ] = useState(true);
+
+  function dropdownShower() {
+    setshowDropdown(!showDropdown);
+  }
+
   return (
   <Container fluid className={styles.categoryBarContainer}>
   <div className="hstack">
   <div className="d-md-none d-sm-none d-none d-lg-block">
 
-  <Dropdown>
-      <Dropdown.Toggle variant="light" id="dropdown-basic">
-      Search By Categories
-      </Dropdown.Toggle>
+    <div className={`${styles.dropdownContainer}`} onClick={dropdownShower}>
+      <button><span>Search By Categories</span> <i className={`${ !showDropdown ? 'fa fa-chevron-up' : 'fa fa-chevron-down' }`}></i></button>
 
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1"><i className='fas fa-mobile-alt'></i> Mobiles</Dropdown.Item>
-        <Dropdown.Item href="#/action-2"><i className="fa fa-laptop"></i> Laptops</Dropdown.Item>
-        <Dropdown.Item href="#/action-3"><i className="fa fa-plug"></i> Electronics</Dropdown.Item>
-        <Dropdown.Item href="#/action-3"><i className='fas fa-bed'></i> Furnitures</Dropdown.Item>
-        <Dropdown.Item href="#/action-3"><i className="fa fa-child"></i> Toys</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">More ...</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+      <div className={`${showDropdown ? styles.dropdownElementsContainer : styles.dropdownElementsContainer_active }`}>
+
+        <div className={`${styles.dropdownElementsInnerContainer}`}>
+                <div>
+                <i className='fas fa-mobile-alt'></i>
+                </div>
+               <div>
+               Mobiles
+               </div>
+        </div>
+
+        <div className={`${styles.dropdownElementsInnerContainer}`}>
+                <div>
+                <i className="fa fa-laptop"></i>
+                </div>
+               <div>
+               Laptops
+               </div>
+        </div>
+
+        <div className={`${styles.dropdownElementsInnerContainer}`}>
+                <div>
+                <i className="fa fa-plug"></i>
+                </div>
+               <div>
+               Electronics
+               </div>
+        </div>
+
+        <div className={`${styles.dropdownElementsInnerContainer}`}>
+                <div>
+                <i className='fas fa-bed'></i>
+                </div>
+               <div>
+               Furnitures
+               </div>
+        </div>
+
+        <div className={`${styles.dropdownElementsInnerContainer}`}>
+                <div>
+                <i className="fa fa-child"></i>
+                </div>
+               <div>
+               Toys
+               </div>
+        </div>
+
+        <div className={`${styles.dropdownElementsInnerContainer}`}>
+                <div>
+                ...
+                </div>
+               <div>
+               More
+               </div>
+        </div>
+
+
+      </div>
+
+    </div>
+
+
   </div>
   <div className={`d-md-none d-sm-none d-none d-lg-block ${styles.categoryButtonsContainer}`}>
     <div>
