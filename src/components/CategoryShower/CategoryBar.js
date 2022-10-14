d36aa78799bb03e306e38e174307fb5e00d6403a
prakash_web_dev <omprakashgwt@gmail.com>
2022-10-14 15:02:10 +0530

    developed individual category displayer component

--- src/components/CategoryShower/CategoryBar.js
+++ src/components/CategoryShower/CategoryBar.js
@@ -1,13 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import styles from './CategoryBar.module.css';
 
+const mobileCategories = {
+  title: "Mobiles",
+  categoriesList: ["Apple", "Realme", "Redmi", "Lenovo", "Oppo", "Nokia", "Samsung", "Sony"]
+}
+
+const laptopCategories = {
+  title: "Laptops",
+  categoriesList: ["Apple", "Acer", "AGB", "Lenovo", "HP", "DEEQ", "Alienware", "Honor"]
+}
+
+const electronicCategories = {
+  title: "Electronics",
+  categoriesList: ["Apple Watch", "Digital Camera", "Earbuds", "Calculator", "Tablets", "PSP", "Iron Box", "Washing Machine"]
+}
+
+const furnitureCategories = {
+  title: "Furniture",
+  categoriesList: ["Chair", "TV Units", "Sofas", "Dining Tables", "Beds", "Bean Bags", "Shoe Racks", "Bulbs"]
+}
+
+const toysCategories = {
+  title: "Toys",
+  categoriesList: ["RC Drones", "RC Helicopters", "Playstations", "Bat", "Football", "Basket Balls", "Spinner", "Board Games"]
+}
+
+
 function CategoryBar() {
 
-  const [showDropdown, setshowDropdown ] = useState(true);
+  const [showDropdown, setshowDropdown ] = useState(false);
+  const [showCategoryRelatedProdList, setShowCategoryRelatedProdList] = useState(false);
+  const [categories, setCategories] = useState(mobileCategories);
 
   function dropdownShower() {
     setshowDropdown(!showDropdown);
+  }
+
+  function categoryRelatedProdListShower(category) {
+    if(category !== "") {
+      setCategories(category);
+      console.log(category);
+    } else {
+      setCategories(categories);
+    }
+    setShowCategoryRelatedProdList(true);
+  }
+
+  function categoryRelatedProdListDisabler() {
+    setShowCategoryRelatedProdList(false);
   }
 
   return (
@@ -16,11 +58,11 @@ function CategoryBar() {
   <div className="d-md-none d-sm-none d-none d-lg-block">
 
     <div className={`${styles.dropdownContainer}`} onClick={dropdownShower}>
-      <button><span>Search By Categories</span> <i className={`${ !showDropdown ? 'fa fa-chevron-up' : 'fa fa-chevron-down' }`}></i></button>
+      <button><span>Search By Categories</span> <i className={`${ showDropdown ? 'fa fa-chevron-up' : 'fa fa-chevron-down' }`}></i></button>
 
-      <div className={`${showDropdown ? styles.dropdownElementsContainer : styles.dropdownElementsContainer_active }`}>
+      <div className={`${!showDropdown ? styles.dropdownElementsContainer : styles.dropdownElementsContainer_active }`} onMouseEnter={() => categoryRelatedProdListShower(categories)} onMouseLeave={categoryRelatedProdListDisabler} >
 
-        <div className={`${styles.dropdownElementsInnerContainer}`}>
+        <div className={`${styles.dropdownElementsInnerContainer}`} onMouseEnter={() => categoryRelatedProdListShower(mobileCategories)} onClick={categoryRelatedProdListDisabler}>
                 <div>
                 <i className='fas fa-mobile-alt'></i>
                 </div>
@@ -29,7 +71,7 @@ function CategoryBar() {
                </div>
         </div>
 
-        <div className={`${styles.dropdownElementsInnerContainer}`}>
+       <div className={`${styles.dropdownElementsInnerContainer}`} onMouseEnter={() => categoryRelatedProdListShower(laptopCategories)} onClick={categoryRelatedProdListDisabler}>
                 <div>
                 <i className="fa fa-laptop"></i>
                 </div>
@@ -38,7 +80,7 @@ function CategoryBar() {
                </div>
         </div>
 
-        <div className={`${styles.dropdownElementsInnerContainer}`}>
+       <div className={`${styles.dropdownElementsInnerContainer}`} onMouseEnter={() => categoryRelatedProdListShower(electronicCategories)} onClick={categoryRelatedProdListDisabler}>
                 <div>
                 <i className="fa fa-plug"></i>
                 </div>
@@ -47,16 +89,16 @@ function CategoryBar() {
                </div>
         </div>
 
-        <div className={`${styles.dropdownElementsInnerContainer}`}>
+       <div className={`${styles.dropdownElementsInnerContainer}`} onMouseEnter={() => categoryRelatedProdListShower(furnitureCategories)} onClick={categoryRelatedProdListDisabler}>
                 <div>
                 <i className='fas fa-bed'></i>
                 </div>
                <div>
-               Furnitures
+               Furniture
                </div>
         </div>
 
-        <div className={`${styles.dropdownElementsInnerContainer}`}>
+       <div className={`${styles.dropdownElementsInnerContainer}`} onMouseEnter={() => categoryRelatedProdListShower(toysCategories)} onClick={categoryRelatedProdListDisabler}>
                 <div>
                 <i className="fa fa-child"></i>
                 </div>
@@ -65,7 +107,7 @@ function CategoryBar() {
                </div>
         </div>
 
-        <div className={`${styles.dropdownElementsInnerContainer}`}>
+       <div className={`${styles.dropdownElementsInnerContainer}`} onMouseEnter={() => categoryRelatedProdListShower(mobileCategories)} onClick={categoryRelatedProdListDisabler}>
                 <div>
                 ...
                 </div>
@@ -74,6 +116,19 @@ function CategoryBar() {
                </div>
         </div>
 
+
+      </div>
+
+      <div className={`${styles.elementsRelatedCategoryContainer} ${showCategoryRelatedProdList ? styles.elementsRelatedCategoryContainer_active : styles.elementsRelatedCategoryContainer_disabled } `} onMouseEnter={() => categoryRelatedProdListShower(categories) } onMouseLeave={categoryRelatedProdListDisabler}  onClick={categoryRelatedProdListDisabler}>
+
+        <div className={`${styles.elementsRelatedCategoryInnerContainer}`}>
+          <span>{categories.title}</span>
+          <ul>
+            { categories.categoriesList.map((category, index) => {
+              return <li key={index}>{category}</li>
+            }) }
+          </ul>
+        </div>
 
       </div>
 
