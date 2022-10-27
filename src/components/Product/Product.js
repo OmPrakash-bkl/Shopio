9af568e5e32012509f0af8d2c27e7bb2045d39d9
prakash_web_dev <omprakashgwt@gmail.com>
2022-10-27 15:56:24 +0530

    developed some product page

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Vivo from '../../assets/bs_mobile.jpg';
 import Vivo1 from '../../assets/vivo1.jpg';
 import Vivo2 from '../../assets/vivo2.jpg';
@@ -7,7 +8,15 @@ import Vivo4 from '../../assets/vivo4.jpg';
 import styles from './Product.module.css';
 
 function Product() {
+
+    const [showReviewBox, setShowReviewBox] = useState(false);
+
+    function reviewBoxShower() {
+        setShowReviewBox(!showReviewBox);
+    }
+
   return (
+    <>
     <div className={`${styles.productParentContainer}`}>
         <div className={`${styles.productImageParentContainer}`}>
             <div className={`${styles.productImageContainer}`}>
@@ -56,11 +65,11 @@ function Product() {
                     </tr>
                     <tr>
                         <th>CATEGORIES: </th>
-                        <td>Mobiles, Tablets</td>
+                        <td><Link to="/" style={{ textDecoration: "none", color: "rgb(110, 110, 110)" }}>Mobiles</Link>, <Link to="/" style={{ textDecoration: "none", color: "rgb(110, 110, 110)" }}>Tablets</Link></td>
                     </tr>
                     <tr>
                         <th>TAGS: </th>
-                        <td>Smart Phones, Trending Mobiles</td>
+                        <td><Link to="/" style={{ textDecoration: "none", color: "rgb(110, 110, 110)" }}>Smart Phones</Link>, <Link to="/" style={{ textDecoration: "none", color: "rgb(110, 110, 110)" }}>Trending Mobiles</Link></td>
                     </tr>
                     <tr>
                         <th>COLOR: </th>
@@ -98,12 +107,90 @@ function Product() {
                 <button>+</button>
             </div>
             <div className={`${styles.socialMediaBtnsContainer}`}>
-                <button className={`${styles.colorBlueBtn}`}><i className="fa fa-facebook"></i> Share</button>
-                <button className={`${styles.colorLiteBlueBtn}`}><i className="fa fa-twitter"></i> Tweet</button>
+                <button className={`${styles.colorBlueBtn}`}><i className="fab fa-facebook-f"></i> SHARE</button>
+                <button className={`${styles.colorLiteBlueBtn}`}><i className="fab fa-twitter"></i> TWEET</button>
                 <button className={`${styles.colorRedBtn}`}>@ PIN IT</button>
             </div>
         </div>
     </div>
+    <div className={`${styles.productDescription}`}>
+        <div className={`${styles.TitleContainer}`}>
+        <h4>PRODUCT DESCRIPTION</h4>
+        </div>
+        <p>Vivo Y22 5G mobiles comes with a 6.50-inch touchscreen display with a resolution of 720x1600 pixels. Vivo Y22 5G is powered by an octo-core MediaTek Dimensity 720 processor. It comes with 6GB of RAM. The Vivo Y22 5G runs Android 11 and is powered by a 5000mAh battery. The Vivo Y22 5G supports proprietary fast charging.</p>
+    </div>
+    <div className={`${styles.productSpecification}`}>
+        <div className={`${styles.TitleContainer}`}>
+        <h4>PRODUCT DESCRIPTION</h4>
+        </div>
+        <table>
+            <tbody className={`${styles.tableHeaderAndElementsContainer}`}>
+                <tr>
+                    <th>In The Box: </th>
+                    <td>Model, Documentation, USB Cable, USB Power Adapter, Sim Eject Tool, Protective Film (Applied), Phone Case</td>
+                </tr>
+                <tr>
+                    <th>Model Name: </th>
+                    <td>Y22</td>
+                </tr>
+                <tr>
+                    <th>Color: </th>
+                    <td>Starlit Blue</td>
+                </tr>
+                <tr>
+                    <th>Display Size: </th>
+                    <td>16.64 cm (6.55 inch)</td>
+                </tr>
+                <tr>
+                    <th>Resolution Type: </th>
+                    <td>HD+</td>
+                </tr>
+                <tr>
+                    <th>Operating System: </th>
+                    <td>Android 12</td>
+                </tr>
+                <tr>
+                    <th>Processor Type: </th>
+                    <td>Mediatek Helio G70</td>
+                </tr>
+                <tr>
+                    <th>Internal Storage: </th>
+                    <td>64 GB</td>
+                </tr>
+                <tr>
+                    <th>RAM: </th>
+                    <td>4 GB</td>
+                </tr>
+                <tr>
+                    <th>Primary Camera: </th>
+                    <td>50MP + 2MP</td>
+                </tr>
+                <tr>
+                    <th>Secondary Camera: </th>
+                    <td>8MP Front Camera</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div className={`${styles.productReviews}`}>
+        <div className={`${styles.TitleContainerReview}`}>
+        <h4>PRODUCT REVIEWS</h4>
+        <button onClick={reviewBoxShower}><i className="fa fa-plus" aria-hidden="true"></i></button>
+        </div>
+        <div className={`${styles.reviewFormContainer} ${showReviewBox ? styles.reviewFormContainerActive : styles.reviewFormContainerDisabler}`}>
+            <form>
+                <input type="text" placeholder="Name" />
+                <input type="text" placeholder="Ratings (Enter Number from 1 to 5)" />
+                <input type="text" placeholder="Description" />
+                <button className={`${styles.reviewContainerBtns}`}>Submit</button>
+                <button className={`${styles.reviewContainerBtns}`}>Cancel</button>
+            </form>
+        </div>
+        <div className={`${styles.prodPeopleReviewContainer}`}>
+
+        </div>
+    </div>
+    </>
   )
 }
 
