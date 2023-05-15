413263b481cdd3bb102ad4fb36cd3b354b01ba5c
omprakash <omprakashgwt@gmail.com>
2023-05-15 15:43:46 +0530

    Updates

---
+++ precache-manifest.66f7ab7fd4043cb71312882cb13b0562.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ae8ebbb755c7c7ff25b",
+    "url": "/static/css/main.2cdecdcc.chunk.css"
+  },
+  {
+    "revision": "8ae8ebbb755c7c7ff25b",
+    "url": "/static/js/main.8ae8ebbb.chunk.js"
+  },
+  {
+    "revision": "89330caa096940b2ff58",
+    "url": "/static/css/1.ad93108d.chunk.css"
+  },
+  {
+    "revision": "89330caa096940b2ff58",
+    "url": "/static/js/1.89330caa.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a135ed0ffd9385147c10818391d1b549",
+    "url": "/static/media/bs_mobile.a135ed0f.jpg"
+  },
+  {
+    "revision": "bc08c3f1193c2e2353f5ab4a5c0dd225",
+    "url": "/static/media/td_camera.bc08c3f1.jpg"
+  },
+  {
+    "revision": "d1b85d35b12f5afaf4c0bf44c35b1809",
+    "url": "/static/media/bs_shoes.d1b85d35.jpg"
+  },
+  {
+    "revision": "d5bb0a2ab9b24a22837bdd1205bc8d3a",
+    "url": "/static/media/bs_laptops.d5bb0a2a.jpg"
+  },
+  {
+    "revision": "3f07c392dadf25888dc0e8a59c676cf2",
+    "url": "/static/media/dammy_dp.3f07c392.jpg"
+  },
+  {
+    "revision": "850372572e387b88f9462692a015ab7d",
+    "url": "/static/media/shoes.85037257.jpg"
+  },
+  {
+    "revision": "5f267b567dad101892690bf61c30ef40",
+    "url": "/static/media/bs_drone.5f267b56.jpg"
+  },
+  {
+    "revision": "452054e0cbdcae6fbc1c289376d40e2c",
+    "url": "/static/media/bs_saree.452054e0.jpg"
+  },
+  {
+    "revision": "bbca0c2aa9d63b556cb41609c58922d1",
+    "url": "/static/media/bs_tv.bbca0c2a.jpg"
+  },
+  {
+    "revision": "0e31c0f4e671a286e635baea96bd8131",
+    "url": "/static/media/bs_tablets.0e31c0f4.jpg"
+  },
+  {
+    "revision": "b91a01f7e11d9f7baaeadb30305a6df2",
+    "url": "/static/media/bs_fridge.b91a01f7.jpg"
+  },
+  {
+    "revision": "87130213e7e450253cec57dcffd5f013",
+    "url": "/static/media/td_mobileCover.87130213.jpg"
+  },
+  {
+    "revision": "03fe3c0338357ce584a4767f1276298c",
+    "url": "/static/media/td_mobileCharger.03fe3c03.jpg"
+  },
+  {
+    "revision": "1d0e982151ff5628d6320af27859ed5f",
+    "url": "/static/media/td_router.1d0e9821.jpg"
+  },
+  {
+    "revision": "25d5078849b55fd956bb0c45982683ef",
+    "url": "/static/media/td_temper.25d50788.jpg"
+  },
+  {
+    "revision": "46210a00d1608f8f63a5de87260a7072",
+    "url": "/static/media/mobiles_cart.46210a00.jpg"
+  },
+  {
+    "revision": "b04fcd98895eb6f554ce464976e0eb45",
+    "url": "/static/media/slide1.b04fcd98.jpg"
+  },
+  {
+    "revision": "2c5e47b03f43d9cbbad211aa9d057591",
+    "url": "/static/media/slide2.2c5e47b0.jpg"
+  },
+  {
+    "revision": "a068a6c65e6ba9ad0cd9aed224cb7edc",
+    "url": "/static/media/slide3.a068a6c6.jpg"
+  },
+  {
+    "revision": "a46f045b7edece2a59bd22af954194e1",
+    "url": "/static/media/laptops.a46f045b.jpg"
+  },
+  {
+    "revision": "d5f71d43b7733028b64cadd36c28ef04",
+    "url": "/static/media/electronics.d5f71d43.jpg"
+  },
+  {
+    "revision": "9147f51795564ab5b74b00c2e503e10b",
+    "url": "/static/media/toys.9147f517.jpg"
+  },
+  {
+    "revision": "91caefd13c1f4b8f05382926d6175191",
+    "url": "/static/media/accesories.91caefd1.jpg"
+  },
+  {
+    "revision": "9849ad74ee2a26d9c17cd42cfc3047a8",
+    "url": "/static/media/speakers.9849ad74.jpg"
+  },
+  {
+    "revision": "4a90e466ab7927b87147a856e0ad2002",
+    "url": "/static/media/housewares.4a90e466.jpg"
+  },
+  {
+    "revision": "3cc50de71e24397bc7133979f8deba6c",
+    "url": "/static/media/gamingConsoles.3cc50de7.jpg"
+  },
+  {
+    "revision": "c0dd4b1fb4f805f9273e12147baf05d5",
+    "url": "/static/media/banner.c0dd4b1f.jpg"
+  },
+  {
+    "revision": "2bb4ace8e9bf0ddec0f3939efc9cf362",
+    "url": "/static/media/tc_furnitures.2bb4ace8.jpg"
+  },
+  {
+    "revision": "ab3561c845875f2c7015401243840102",
+    "url": "/static/media/tc_cloths.ab3561c8.jpg"
+  },
+  {
+    "revision": "59283f9f409db2de3cf143377cac73d5",
+    "url": "/static/media/tc_bags.59283f9f.jpg"
+  },
+  {
+    "revision": "88c098ace5db39ac69533871f4fb5bc7",
+    "url": "/static/media/banner2.88c098ac.jpg"
+  },
+  {
+    "revision": "67b6165c17ac06c93fdd2ff4679cdc78",
+    "url": "/static/media/favicon.67b6165c.png"
+  },
+  {
+    "revision": "c0461fb6ab1b7b1f11dd5ea27237f01a",
+    "url": "/static/media/productPageBanner.c0461fb6.jpg"
+  },
+  {
+    "revision": "0a99915e0bab6839922a5d3e36ee2815",
+    "url": "/static/media/vivo1.0a99915e.jpg"
+  },
+  {
+    "revision": "3b180a811150f79d8cf747d996846ecd",
+    "url": "/static/media/vivo2.3b180a81.jpg"
+  },
+  {
+    "revision": "837dadc6f400da4be0e7e4e7098078ba",
+    "url": "/static/media/vivo3.837dadc6.jpg"
+  },
+  {
+    "revision": "c884b33e765afb966d97c9356f88fd81",
+    "url": "/static/media/not_found_image2.c884b33e.png"
+  },
+  {
+    "revision": "f60cc8b3fb9d45ac5e588db01c50617b",
+    "url": "/index.html"
+  }
+];
